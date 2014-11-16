70c6295be13b6877de9f5e67347e87e5ffbcfa51
Trevor Overman <troverman@gmail.com>
2014-11-16 10:14:42 -0500

    lol

--- index.js
+++ index.js
@@ -70,13 +70,11 @@ function sendMail(buyers){
 		var email = new sendgrid.Email();
 		email.addTo(to);
 		email.setFrom(to);
-		email.setSubject('[sendgrid-php-example] Owl');
-		email.setText('Owl are you doing?');
-		email.setHtml('<strong>%how% are you doing?</strong>');
-		email.addSubstitution("%how%", "Owl");
+		email.setSubject('food point -- ');
+		email.setText('this is an email to the buyers');
+		email.setHtml('<strong>what is going on yo</strong>');
 		email.addHeader('X-Sent-Using', 'SendGrid-API');
 		email.addHeader('X-Transport', 'web');
-		//email.addFile({path: './gif.gif', filename: 'owl.gif'});
 
 		sendgrid.send(email, function(err, json) {
 		if (err) { return console.error(err); }
