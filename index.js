9a55b91d64cea9a1072f8223021f39a4d9171a12
Trevor Overman <troverman@gmail.com>
2014-11-16 03:27:49 -0500

    lol

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ var myFirebaseRef = new Firebase("https://shining-heat-3529.firebaseio.com/buyer
 // Retrieve new posts as they are added to Firebase
 ref.on("child_added", function(snapshot) {
 	var newPost = snapshot.val();
-	console.log("Author: " + newPost.email);
-	console.log("Title: " + newPost.trans);
 });
 
 app.set('port', (process.env.PORT || 5000))
