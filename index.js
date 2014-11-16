44a2deb5b74755b5ddde04d73e7312386f3f08e1
Trevor Overman <troverman@gmail.com>
2014-11-16 04:08:33 -0500

    lol

--- index.js
+++ index.js
@@ -29,11 +29,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send(newPost)
-})
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+  response.send('newPost')
 })
 
 
