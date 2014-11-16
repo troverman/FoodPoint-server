803d706fba20529c93dfd5d459814076fc1897a6
Trevor Overman <troverman@gmail.com>
2014-11-16 02:53:52 -0500

    lol

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ var express = require('express')
 var app = express();
 
 
-var myFirebaseRef = new Firebase("https://shining-heat-3529.firebaseio.com/");
+//var myFirebaseRef = new Firebase("https://shining-heat-3529.firebaseio.com/");
 
-myFirebaseRef.child("buyers").on("value", function(snapshot) {
-    alert(snapshot.val()); // Alerts "San Francisco"
-});
+//myFirebaseRef.child("buyers").on("value", function(snapshot) {
+    //alert(snapshot.val()); // Alerts "San Francisco"
+//});
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send(snapshot.val())
+  response.send('test')
 })
 
 app.listen(app.get('port'), function() {
