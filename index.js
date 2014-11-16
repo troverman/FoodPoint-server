c9835c40f5e17c878ef4326146c6fbf3c91c1703
Trevor Overman <troverman@gmail.com>
2014-11-16 02:51:57 -0500

    lol

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express')
+var app = express();
+
+
+var myFirebaseRef = new Firebase("https://shining-heat-3529.firebaseio.com/");
+
+myFirebaseRef.child("buyers").on("value", function(snapshot) {
+    alert(snapshot.val()); // Alerts "San Francisco"
+});
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  response.send(snapshot.val())
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
