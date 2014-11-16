4c5168597591a259a2a193ef5f5c0c34d3b7fee0
Trevor Overman <troverman@gmail.com>
2014-11-16 03:27:08 -0500

    lol

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ var express = require('express')
 var app = express();
 var Firebase = require("firebase");
 
-var myFirebaseRef = new Firebase("https://shining-heat-3529.firebaseio.com/");
+var myFirebaseRef = new Firebase("https://shining-heat-3529.firebaseio.com/buyers");
 
-
+// Retrieve new posts as they are added to Firebase
+ref.on("child_added", function(snapshot) {
+	var newPost = snapshot.val();
+	console.log("Author: " + newPost.email);
+	console.log("Title: " + newPost.trans);
+});
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
