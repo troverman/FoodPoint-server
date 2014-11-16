db3961d4514f4adf82bbd8a06d30d557330935dd
Matt O'Hagan <mco13@my.fsu.edu>
2014-11-16 11:00:20 -0500

    final logic?

--- index.js
+++ index.js
@@ -5,10 +5,16 @@ var Firebase = require("firebase");
 var baseURL = "https://shining-heat-3529.firebaseio.com/";
 var rootRef = new Firebase(baseURL);
 var marketsRef = rootRef.child('markets');
+var requestRef = rootRef.child('requests');
 
+
+// watch for new markets
 marketsRef.orderByKey().on("child_added", function(snapshot) {	
 	var marketInfo = snapshot.val();
 	//console.log(marketInfo);
+
+	console.log(marketInfo.date);
+	console.log(marketInfo.lon);
 
 	var marketRef = marketsRef.child(snapshot.key());
 	var sellerList = marketInfo.sellers;
@@ -17,7 +23,7 @@ marketsRef.orderByKey().on("child_added", function(snapshot) {
 	if(Object.size(sellerList) >= 5) {
 		var emailed = snapshot.val().emailed;
 		if(emailed == "no"){
-			sendMail(buyers);
+			sendMail(buyers, marketInfo);
 			marketRef.update({
 				"emailed": "yes"
 			});
@@ -26,7 +32,7 @@ marketsRef.orderByKey().on("child_added", function(snapshot) {
 	}	
 });
 
-
+// watch for change in markets
 marketsRef.orderByKey().on("child_changed", function(snapshot) {
 	var marketInfo = snapshot.val();
 	//console.log(marketInfo);
@@ -38,13 +44,40 @@ marketsRef.orderByKey().on("child_changed", function(snapshot) {
 	if(Object.size(sellerList) >= 5) {
 		var emailed = snapshot.val().emailed;
 		if(emailed == "no"){
-			sendMail(buyers);
+			sendMail(buyers, marketInfo);
 			marketRef.update({
 				"emailed": "yes"
 			});
 			console.log("email sent");
 		}
 	}
+}); 
+
+// watch for change in currentRequest
+requestRef.orderByKey().on("child_changed", function(snapshot) {
+	var request = snapshot.val();
+	console.log(request);
+	//console.log(marketInfo);
+
+	var sendgrid_username = 'troverman';
+	var sendgrid_password = 'trev77922';
+	var to = request.email;
+	var sendgrid = require('sendgrid')(sendgrid_username, sendgrid_password);
+	var email = new sendgrid.Email();
+	email.addTo(to);
+	email.setFrom('troverman@gmail.com');
+	email.setSubject('Foodpoint');
+	email.setHtml('<h2>Hey ' + request.name + ',</h2><p>Glad you\'re on board with Foodpoint and promoting local farmers! We\'ll let you know when ' + request.nameOfMarket + ' is open!</p><br>');
+	email.addHeader('X-Sent-Using', 'SendGrid-API');
+	email.addHeader('X-Transport', 'web');
+
+	sendgrid.send(email, function(err, json) {
+	if (err) { return console.error(err); }
+		console.log(json);
+	}); 
+
+
+
 }); 
 
 
@@ -58,7 +91,7 @@ Object.size = function(obj) {
 }; 
 
 
-function sendMail(buyers){
+function sendMail(buyers, market){
 	// run through buyer list
 	for(key in buyers) {
 		var toEmail = buyers[key]['email'];
@@ -69,9 +102,9 @@ function sendMail(buyers){
 		var sendgrid = require('sendgrid')(sendgrid_username, sendgrid_password);
 		var email = new sendgrid.Email();
 		email.addTo(to);
-		email.setFrom(to);
-		email.setSubject('food point');
-		email.setHtml('<p>glad youre on board with foodpoint and promoting local farmers!</p><br><hr><br><p>date: 11/11/11</p><br><hr><br><p>location: lat lng </p><br><hr><br>');
+		email.setFrom('troverman@gmail.com');
+		email.setSubject('Foodpoint');
+		email.setHtml('<p>Glad you\'re on board with Foodpoint and promoting local farmers! The Durham Farmer\'s Market is happening!</p><br><hr><br><p>Date: ' + market.date + '</p><br><hr><br><p>Location: lat: ' + market.lat + ' long:' + market.lon + '  </p><br><hr><br>');
 		email.addHeader('X-Sent-Using', 'SendGrid-API');
 		email.addHeader('X-Transport', 'web');
 
