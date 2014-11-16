b8542b6e7a5533926d52c5058b0c2d2d22cfdf35
Trevor Overman <troverman@gmail.com>
2014-11-16 03:32:29 -0500

    lol

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('test')
+  response.send(snapshot.val())
 })
 
 app.listen(app.get('port'), function() {
